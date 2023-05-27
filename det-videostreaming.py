495d20b0c32b9378afac532a4c82f1a8be1aaef4
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-05-27 16:33:14 +0800

    Update det-videostreaming.py

--- det-videostreaming.py
+++ det-videostreaming.py
@@ -8,6 +8,7 @@ import torch
 from config import CLASSES, COLORS
 from models.torch_utils import det_postprocess
 from models.utils import blob, letterbox, path_to_list
+import threading
 
 def main():
 	device = torch.device('cuda:0')
